6ac93f9c6783ac38e9f3e57e7b738a284aebd7a5
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-25 10:37:46 -0700

    add webpack.config file content

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,67 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const { GenerateSW } = require("workbox-webpack-plugin");
+const path = require("path");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      cards: "./src/js/cards.js",
     },
 
     // TODO: Add the correct output
     output: {
-      
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Mini Project PWA",
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve("src/assets/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512], 
+          },
+        ],
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
   };
 };
